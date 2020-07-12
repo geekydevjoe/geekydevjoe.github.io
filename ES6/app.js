061b70488112e5ff37944a9930a07f2ccb60b1d3
Joe Graf <geekydevjoe@gmail.com>
2020-07-12 11:53:34 -0700

    .

--- ES6/app.js
+++ ES6/app.js
@@ -4,6 +4,6 @@ import Expedition from './expedition.js'
 import { print, log } from './text-helpers.js'
 
 
-//const trip = new Expedition("Mt. Whitney", 3, ["sunglasses", "prayer flags"])
-//trip.print()
-//freel.print()
+const trip = new Expedition("Mt. Whitney", 3, ["sunglasses", "prayer flags"])
+trip.print()
+freel.print()
