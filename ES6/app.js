71af6e5bcba1a90db94c363205b3e2b7ac494678
Joe Graf <geekydevjoe@gmail.com>
2020-07-12 11:51:48 -0700

    .

--- ES6/app.js
+++ ES6/app.js
@@ -1,7 +1,7 @@
 import Vacation from './vacation.js'     
 import Expedition from './expedition.js'     
 //import freel from './mt-freel.js'
-import { pring, log } from './text-helpers.js'
+import { print, log } from './text-helpers.js'
 
 
 //const trip = new Expedition("Mt. Whitney", 3, ["sunglasses", "prayer flags"])
