628ac6ade5ada66e5f657f9aa2375a8ccca2f41a
Joe Graf <geekydevjoe@gmail.com>
2020-07-13 20:47:08 -0700

    more scorecard updates

---
+++ scorecard/precache-manifest.999fc250b3e697c6f77030e87346399e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c484c172f784016138c436fc0f5baeb",
+    "url": "/scorecard/index.html"
+  },
+  {
+    "revision": "611a15109f049ae2d10d",
+    "url": "/scorecard/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "2b51681d96bc08d60487",
+    "url": "/scorecard/static/css/main.cc94d8c7.chunk.css"
+  },
+  {
+    "revision": "611a15109f049ae2d10d",
+    "url": "/scorecard/static/js/2.cbfe9c8b.chunk.js"
+  },
+  {
+    "revision": "618d9e067b5f7c0ee048a43ae1109b7d",
+    "url": "/scorecard/static/js/2.cbfe9c8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b51681d96bc08d60487",
+    "url": "/scorecard/static/js/main.c56e9064.chunk.js"
+  },
+  {
+    "revision": "3ff787b2ea7e1b7248ed",
+    "url": "/scorecard/static/js/runtime-main.4b9dfbc8.js"
+  }
+]);
--- scorecard/service-worker.js
+++ scorecard/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.09e1b2bbdaf7ff50bf044fe63cef6bd7.js"
+  "/scorecard/precache-manifest.999fc250b3e697c6f77030e87346399e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/scorecard/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
