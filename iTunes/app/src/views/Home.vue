7e065c899572fb5d3c9afe2c06eb85996bd1a1e3
geekydevjoe <geekydevjoe@gmail.com>
2021-10-24 20:08:16 -0700

    .

--- iTunes/app/src/views/Home.vue
+++ iTunes/app/src/views/Home.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="home">
     <form @submit="searchItunes(searchText)">
-      </form>
       <input v-model="searchText"  placeholder="search for album"/>
+      </form>
+
       <div v-if="data.resultCount > 0">
          <div v-for="album in data.results" :key="album.collectionName">
            <h3>{{ album.collectionName }}</h3>
@@ -17,7 +18,6 @@
 import { ItunesTypes } from '../types/itunesTypes';
 import { defineComponent } from 'vue';
 import { itunesSearch } from '../services/iTunesApi';
-import { ApiClientType } from '../types/ApiClient';
 
 declare var ApiClient: any;
 
@@ -28,13 +28,11 @@ export default defineComponent({
   data: () => {
     return {
       data: {} as ItunesTypes,
-      searchText: "",
-      client: {} as ApiClientType
+      searchText: ""
     }
   },
   mounted(){
     console.log("mounted");
-    this.client = new ApiClient();
   },
   methods: {
     async searchItunes(search:string){
@@ -42,10 +40,6 @@ export default defineComponent({
       const value = await itunesSearch(search);
       this.data = value;
       console.log("data", value);
-      console.log(this.client.getUrlAndQuery());
-      // this.client.action("joe").data(function(response:any){
-      //   console.log("all done");
-      // });
     }
   }
 });
