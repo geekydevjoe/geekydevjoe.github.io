9f0e5f850088e54c09e69fb245239d12fcfc1a4c
geekydevjoe <geekydevjoe@gmail.com>
2021-10-24 20:29:54 -0700

    .

--- iTunes/app/src/services/iTunesApi.ts
+++ iTunes/app/src/services/iTunesApi.ts
@@ -2,5 +2,9 @@ import { ItunesTypes } from "@/types/itunesTypes";
 
 export const itunesSearch = async (search:string):Promise<ItunesTypes> =>
     await fetch(
-        `https://itunes.apple.com/search?term=${search}&entity=album`
+        `https://itunes.apple.com/search?term=${search}&entity=podcast`,{
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+             }
+        }
     ).then(val => val.json());
