0b58df41fcc72512ad7578816495df63be58298e
Joe Graf <geekydevjoe@gmail.com>
2021-10-26 18:43:40 -0700

    .

---
+++ iTunes/app/src/components/PodcastCard.vue
@@ -0,0 +1,22 @@
+/* eslint-disable */
+<template>
+<div>
+  i am a card 
+</div>
+</template>
+
+<script lang="ts">
+import { ItunesTypes, Album } from '../types/itunesTypes';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PodcastCard',
+  props: {
+  }
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
